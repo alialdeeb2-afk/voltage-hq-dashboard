dae671e245a0b35699dac90e2c679559bc9369cc
alialdeeb2-afk <alialdeeb2@gmail.com>
2025-08-02 23:45:28 +0300

    Add files via upload

---
+++ voltage-hq-dashboard-main/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './components/AuthContext';
+import Login from './components/Login';
+import RequestForm from './components/RequestForm';
+import AdminPanel from './components/AdminPanel';
+import HQDashboard from './components/HQDashboard';
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter basename="/voltage-hq-dashboard">
+        <Routes>
+          <Route path="/login"   element={<Login />} />
+          <Route path="/request" element={<RequestForm />} />
+          <Route path="/admin"   element={<AdminPanel />} />
+          <Route path="/hq"      element={<HQDashboard />} />
+          <Route path="*"        element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
