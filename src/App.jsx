4a2b8373543643af7d3d2277777dbb25a48d12c2
alialdeeb2-afk <alialdeeb2@gmail.com>
2025-08-02 16:27:08 +0300

    Populate App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HQDashboard from './components/HQDashboard';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HQDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
