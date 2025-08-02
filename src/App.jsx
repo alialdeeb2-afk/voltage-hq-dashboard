2c41951e35b01d33aa436f27da14ad907e30b6cf
alialdeeb2-afk <alialdeeb2@gmail.com>
2025-08-02 21:14:34 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HQDashboard from './components/HQDashboard';
+-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
++import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HQDashboard />} />
-      </Routes>
-    </BrowserRouter>
+-    <AuthProvider>
+-      <BrowserRouter>
++    <AuthProvider>
++      {/* ← tell React Router about the repo sub-folder */}
++      <BrowserRouter basename="/voltage-hq-dashboard">
+        <Routes>
+          <Route path="/login"   element={<Login />} />
+          <Route path="/request" element={<RequestForm />} />
+          <Route path="/admin"   element={<AdminPanel />} />
+          <Route path="/hq"      element={<HQDashboard />} />
+          <Route path="*"        element={<Navigate to="/login" replace />} />
+        </Routes>
+-      </BrowserRouter>
+-    </AuthProvider>
++      </BrowserRouter>
++    </AuthProvider>
   );
 }
-
